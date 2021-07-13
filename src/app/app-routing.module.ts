0692292bbef98d4f7366c0576490fa3496d73e4e
unknown <julesnematchou@gmail.com>
2021-07-13 10:09:50 +0200

    Update component files & delete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { AProposComponent } from './a-propos/a-propos.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { GesUsersGuard } from './Guard/ges-users.guard';
 import { LoginComponent } from './login/login.component';
 import { ListTickectComponent } from './list-tickect/list-tickect.component';
 import { FaqComponent } from './faq/faq.component';
 import { EditCategorieComponent } from './edit-categorie/edit-categorie.component';
-import { GesCategorieComponent } from './ges-categorie/ges-categorie.component';
-import { AddCategorieComponent } from './add-categorie/add-categorie.component';
 import { GesUsersComponent } from './ges-users/ges-users.component';
 import { AddUserComponent } from './add-user/add-user.component';
-import { EditTicketComponent } from './edit-ticket/edit-ticket.component';
-import { DeleteTicketComponent } from './delete-ticket/delete-ticket.component';
 import { AddTickectComponent } from './add-tickect/add-tickect.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
@@ -20,18 +15,14 @@ import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
   { path: 'home', component: HomeComponent, canActivate:[AuthGuard] },
   { path: 'add-ticket', component: AddTickectComponent, canActivate:[AuthGuard] },
-  { path: 'delete-ticket', component: DeleteTicketComponent, canActivate:[AuthGuard] },
-  { path: 'edit-ticket', component: EditTicketComponent, canActivate:[AuthGuard] },
   { path: 'add-user', component: AddUserComponent, canActivate:[AuthGuard] },
   { path: 'faq', component: FaqComponent, canActivate:[AuthGuard] },
   { path: 'ges-users', component: GesUsersComponent, canActivate:[AuthGuard] },
-  { path: 'add-categorie', component: AddCategorieComponent, canActivate:[AuthGuard] },
-  { path: 'ges-categorie', component: GesCategorieComponent, canActivate:[AuthGuard] },
   { path: 'edit-categorie', component: EditCategorieComponent, canActivate:[AuthGuard] },
   { path: 'list-ticket', component: ListTickectComponent, canActivate:[AuthGuard] },
   { path: 'a-propos', component: AProposComponent, canActivate:[AuthGuard]},
-  // { path: '**', component: PageNotFoundComponent },
-  { path: 'login', component: LoginComponent},
+  { path: '', component: LoginComponent},
+  { path: '**', component: PageNotFoundComponent },
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddTickectComponent } from './add-tickect/add-tickect.component';
-import { EditTicketComponent } from './edit-ticket/edit-ticket.component';
-import { DeleteTicketComponent } from './delete-ticket/delete-ticket.component';
 import { ListTickectComponent } from './list-tickect/list-tickect.component';
-import { EditUserComponent } from './edit-user/edit-user.component';
 import { GesUsersComponent } from './ges-users/ges-users.component';
-import { AddCategorieComponent } from './add-categorie/add-categorie.component';
 import { EditCategorieComponent } from './edit-categorie/edit-categorie.component';
-import { GesCategorieComponent } from './ges-categorie/ges-categorie.component';
 import { HomeComponent } from './home/home.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { FaqComponent } from './faq/faq.component';
@@ -18,27 +13,20 @@ import { AProposComponent } from './a-propos/a-propos.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FormsModule } from '@angular/forms';
-import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddTickectComponent,
-    EditTicketComponent,
-    DeleteTicketComponent,
     ListTickectComponent,
-    EditUserComponent,
     GesUsersComponent,
-    AddCategorieComponent,
     EditCategorieComponent,
-    GesCategorieComponent,
     HomeComponent,
     AddUserComponent,
     FaqComponent,
     AProposComponent,
     LoginComponent,
     PageNotFoundComponent,
-    MenuComponent,
 
 
   ],
